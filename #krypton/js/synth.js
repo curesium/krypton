c6e796da43e82cf5874564dbd91494229de91a8e
Thijs Boom <twboom@hotmail.nl>
2020-11-17 00:07:50 +0100

    After too much fiddeling around, here's a working beep generator

--- #krypton/js/synth.js
+++ #krypton/js/synth.js
@@ -5,13 +5,24 @@ function synth() {
   oscillator = interface.createOscillator();
 
   // Defining the type of oscillator
-  oscillator.type = 'sine';
+  oscillator.type = "sine";
   oscillator.frequency.value = 440;
-    
 
+  // Create volume
+  const gainNode = interface.createGain();
+
+  // Set the attack
+  gainNode.gain.exponentialRampToValueAtTime(0.00001, interface.currentTime + 1);
+
+  // Connent the elements
+  oscillator.connect(gainNode);
+  gainNode.connect(interface.destination);
+
+  // Start the oscillator
+  oscillator.start(0);
 }
 
 
 // For the play button
 let play = document.getElementById('playButton');
-play.addEventListener('click', synth)
+play.addEventListener('mousedown', synth);
