a2ac41a663cfabed820520aefa09aca75706d4cc
Thijs Boom <twboom@hotmail.nl>
2020-11-16 23:45:50 +0100

    Added play button functionality in JS

--- #krypton/js/synth.js
+++ #krypton/js/synth.js
@@ -1,5 +1,10 @@
 const interface = new AudioContext();
 
 function synth() {
-  
+
 }
+
+
+// For the play button
+let play = document.getElementById('playButton');
+play.addEventListener('click', synth)
